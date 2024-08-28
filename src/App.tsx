d57b5ad34401c1e24e2b7fd7129ebde507bc170b
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-28 07:50:08 +0200

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import BookTest from "./BookTest";
+import React, { useState, useMemo } from "react";
+import { BookForm } from "./components/BookForm/BookForm";
+import { BookList } from "./components/BookList/BookList";
+import { GenreFilter } from "./components/GenreFilter/GenreFilter";
+import { Book, FormData } from "./types";
 
-const App = () => {
-  return <BookTest />;
+const App: React.FC = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [activeGenre, setActiveGenre] = useState("");
+
+  const addBook = (bookData: FormData) => {
+    setBooks((prevBooks) => [...prevBooks, { ...bookData, id: Date.now() }]);
+  };
+
+  const genres = useMemo(() => {
+    return Array.from(new Set(books.map((book) => book.genre)));
+  }, [books]);
+
+  const filteredBooks = useMemo(() => {
+    return activeGenre
+      ? books.filter((book) => book.genre === activeGenre)
+      : books;
+  }, [books, activeGenre]);
+
+  return (
+    <div>
+      <h1>Book Management App</h1>
+      <BookForm onSubmit={addBook} />
+      <GenreFilter
+        genres={genres}
+        activeGenre={activeGenre}
+        onGenreChange={setActiveGenre}
+      />
+      <BookList books={filteredBooks} />
+    </div>
+  );
 };
 
 export default App;
